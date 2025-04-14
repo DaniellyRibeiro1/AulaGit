1a8d2c3916aa167efec70b7b66dbae6193ce9af5
Danielly <ribeirodanielly1608@gmail.com>
2025-04-14 10:52:15 -0300

    Aula 13 e aula 14

--- git.js
+++ git.js
@@ -1,3 +1,6 @@
 let comidas = ['Lasanha', 'Pizza', 'Sushi', 'Hamburguer'];
 console.log(`Lista de todas as comidas: ${comidas} `);
 console.log|(`Primeira comida: ${comidas[0]}`);
+comidas.sort();
+console.log(`Comidas ordenadas: ${comidas}`);
+console.log('FIM')
